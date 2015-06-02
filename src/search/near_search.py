3ad60759cd367cc1ad4855434fbb6599a59a1e3a
hadoop <qiulei.guo@hotmail.com>
2015-06-02 13:16:46 -0400

    initialize json

--- src/search/near_search.py
+++ src/search/near_search.py
@@ -5,6 +5,12 @@ Created on Jun 2, 2015
 '''
 
 import urllib.request
+import json
 
 x = urllib.request.urlopen('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=5000&key=AIzaSyBxODBMe80kjYwRjNUTY6FymkJ7eHFhWs8')
-print(x.read().decode('utf-8'))
+j_obj = json.loads(x.read().decode('utf-8'))
+for j in j_obj['results']:
+    for type in j['types']:
+        print(type,end=' ')
+    print()
+        
