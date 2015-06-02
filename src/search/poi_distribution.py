d85e6f053c9aea8b0a509287ff279294ac84e378
batilei <qiulei.guo@hotmail.com>
2015-06-02 17:12:36 -0400

    with google place type convert

---
+++ src/search/poi_distribution.py
@@ -0,0 +1,18 @@
+
+
+def poi_type_map(file):
+    f = open(file,'r')
+    
+    poi_convert = {}
+    for line in f:
+        line = line[0:-1]
+        strs = line.split("\t")
+        poi_convert[strs[0]] = strs[1]
+    
+    return poi_convert
+    
+
+    
+poi_convert = poi_type_map("../../gplace_type_map.txt")
+for key in poi_convert:
+    print(key,poi_convert[key])
